2bc08884c5fad9bf3a210ee7a9a8b2a0fef1450f
kcbry <kcbryan10@gmail.com>
2022-02-20 14:39:53 -0600

    updated server.js file

--- server/server.js
+++ server/server.js
@@ -3,8 +3,20 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const {authMiddleware} = require ('./utils/auth');
+const { typeDefs, resolvers } = require ('./schema')
+const { ApolloServer} = require ('apollo-server-express')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +26,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
