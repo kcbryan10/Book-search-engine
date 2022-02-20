e91b4fbaa90f9c327e8b2c06592e20f9ee2c873e
kcbry <kcbryan10@gmail.com>
2022-02-20 16:26:01 -0600

    updated searchbooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Jumbotron,Container,CardColumns,Card, Button,} from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/react-hooks";
-
 import Auth from "../utils/auth";
 import { removeBookId, saveBookIds } from "../utils/localStorage";
 import { GET_ME } from "../utils/queries";
